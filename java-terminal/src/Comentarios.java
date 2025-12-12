86b1a8ee276e89cf07b9546edb9efd537bda87c7
Soarezzsemj <popcornor6topster@outlook.com>
2025-12-12 14:43:02 -0300

    .

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    // comentario de uma linha
+    
+    /* comentario em mais de uma linha
+     */
+
+
+
+/** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+
+public void metodo(){
+
+
+} 
+}
